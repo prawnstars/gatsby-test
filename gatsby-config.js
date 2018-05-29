b2f8d8c81176cc0588d208e4ac7ed66cd563d4a3
Allard van Helbergen <allard@vanhelbergen.com>
2018-05-30 00:42:22 +1000

    Add google analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,18 @@ module.exports = {
   siteMetadata: {
     title: 'Roar - Voice Assitant for Jira',
   },
-  pathPrefix: `/gatsby-test`,
+  pathPrefix: '/gatsby-test',
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-101598482-2',
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
   ],
 }
