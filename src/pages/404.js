2df9411695d22d9c1f376bafd3f47c9a42eebcc9
Allard van Helbergen <allard@vanhelbergen.com>
2018-06-06 23:55:57 +1000

    remove old files. update 404.

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,45 @@ import React from 'react'
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div class="overlay"></div>
+    <div class="masthead">
+      <div class="masthead-bg"></div>
+      <div class="container h-100">
+        <div class="row h-100">
+          <div class="col-12 my-auto">
+            <div class="masthead-content text-white py-5 py-md-0">
+              <div class="logo">
+                <img src="img/logo-roar.svg" class="logo-image" />
+                <span class="logo-text">Roar</span>
+              </div>
+              <h2>Page Not Found</h2>
+              <p class="mb-5">
+                Sorry, but that page doesn't exist.
+              </p>
+              <p class="mb-5">
+                You should check out our <a href="/">voice assistant</a>, though.
+                It's pretty cool.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="social-icons">
+      <ul class="list-unstyled text-center mb-0">
+        <li class="list-unstyled-item">
+          <a href="#">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </li>
+        <li class="list-unstyled-item">
+          <a href="#">
+            <i class="fa fa-envelope"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 )
 
